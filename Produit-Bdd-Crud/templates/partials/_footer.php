5dd00e00350fc4782b2a5e679097cbe6efea7643
unknown <rodrigue.botte@gmail.com>
2025-07-02 15:32:00 +0200

    modif header en boostraps

--- Produit-Bdd-Crud/templates/partials/_footer.php
+++ Produit-Bdd-Crud/templates/partials/_footer.php
@@ -14,3 +14,8 @@
 </html>
 
 <script src="https://kit.fontawesome.com/2ea3b82358.js" crossorigin="anonymous"></script>
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
