0de2f8df6e4a5f5de735c92a5012cef6874377ea
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-07-03 16:24:11 +0200

    mise en place du formulaire pour ajouter un élève

---
+++ PhpNative/templates/ajouterEleve.php
@@ -0,0 +1,37 @@
+<?php
+require_once './partials/_head.php';
+require_once './partials/_navBar.php'
+
+?>
+
+<h1>Ajouter un élève</h1>
+<form action="../src/controller/ajouterEleveController.php" method="post" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="nom" class="form-label">Nom de l'élève : </label>
+        <input type="text" class="form-control" id="nom" aria-describedby="nomEleve">
+    </div>
+    <div class="mb-3">
+        <label for="prenom" class="form-label">Prénom de l'élève : </label>
+        <input type="text" class="form-control" id="prenom">
+    </div>
+    <div class="mb-3">
+        <label for="age" class="form-label">Age de l'élève : </label>
+        <input type="number" class="form-control" id="age">
+    </div>
+    <div class="mb-3">
+        <label for="dateNaissance" class="form-label">Date de naissance de l'élève : </label>
+        <input type="date" class="form-control" id="dateNaissance">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email de l'élève : </label>
+        <input type="email" class="form-control" id="email">
+    </div>
+    <div class="mb-3">
+        <label for="ville" class="form-label">Ville de l'élève : </label>
+        <input type="text" class="form-control" id="ville">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+require_once './partials/_footer.php';
+?>
--- PhpNative/templates/partials/_navBar.php
+++ PhpNative/templates/partials/_navBar.php
@@ -15,6 +15,9 @@
                 <li class="nav-item">
                     <a class="nav-link text-white" href="../../test/testConnexion.php">Test BDD</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../../templates/ajouterEleve.php">Ajouter un élève</a>
+                </li>
             </ul>
         </div>
     </div>
